1b6d37b5f0e976f22600951a4f2593754e5a5135
Dovalkin <tranquangtuananh2801@gmail.com>
2021-09-24 13:07:46 +1000

    update

---
+++ Haunted Mansion on a hill/Assets/Scripts/Flashlight.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+
+    [SerializeField] GameObject flashLightLight;
+    private bool FlashlightActive = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        flashLightLight.gameObject.SetActive(false); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (FlashlightActive == false)
+            {
+                flashLightLight.gameObject.SetActive(true);
+                FlashlightActive = true;
+            }
+            else
+            {
+                flashLightLight.gameObject.SetActive(false);
+                FlashlightActive = false;
+            }
+        }
+    }
+}
